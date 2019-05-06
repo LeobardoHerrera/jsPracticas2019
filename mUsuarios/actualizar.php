b0db15467aadd132772c9eb9733eadef6b3add4e
LeobardoHerrera <leova-lht@live.com.mx>
2019-05-05 22:17:26 -0500

    Mi primer commit

---
+++ mUsuarios/actualizar.php
@@ -0,0 +1,30 @@
+<?php
+//se manda llamar la conexion
+include("../conexion/conexion.php");
+
+
+//RECIBE VARIABLES AJAX
+$usuario = $_POST["usuario"];
+$contra  = $_POST["contra"];
+$ide     = $_POST["ide"];
+
+$usuario    =trim($usuario);
+$contra   =trim($contra);
+
+
+$fecha=date("Y-m-d"); 
+$hora=date ("H:i:s");
+
+mysql_query("SET NAMES utf8");
+ $insertar = mysql_query("UPDATE usuarios SET  
+ 								
+ 								usuario='$usuario',
+ 								contra='$contra',
+ 								fecha_registro='$fecha',
+ 								hora_registro='$hora',
+ 								id_registro='1'
+ 								
+								WHERE id_usuario='$ide'
+							",$conexion)or die(mysql_error());
+
+?>
---
+++ mUsuarios/restaurarContra.php
@@ -0,0 +1,23 @@
+<?php
+//se manda llamar la conexion
+include("../conexion/conexion.php");
+
+//RECIBE VARIABLES AJAX
+$idUser    = $_POST["idUser"];
+
+
+$fecha =date("Y-m-d"); 
+$hora  =date ("H:i:s");
+
+mysql_query("SET NAMES utf8");
+ $insertar = mysql_query("UPDATE usuarios SET  
+ 								
+ 								contra='12345',
+ 								fecha_registro='$fecha',
+ 								hora_registro='$hora',
+ 								id_registro='1'
+ 								
+								WHERE id_usuario='$idUser'
+							",$conexion)or die(mysql_error());
+
+?>
